b636302d4f03d2cf7815a08be59b492834af1428
Anosha993 <anosharana196@gmail.com>
2026-06-26 21:31:08 +0500

    Scrape all books on one page

--- scraper.py
+++ scraper.py
@@ -1,17 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
 
-# Launch Chrome
+
 driver = webdriver.Chrome()
 
-# Open the website
 driver.get("https://books.toscrape.com/")
 
-# Print the page title
-print(driver.title)
+books = driver.find_elements(By.CLASS_NAME, "product_pod")
 
-# Keep the browser open for 5 seconds
-import time
-time.sleep(5)
+books_data = []
 
-# Close the browser
+for book in books:
+   title = book.find_element(By.CSS_SELECTOR, "h3 a").get_attribute("title")
+   price = book.find_element(By.CLASS_NAME, "price_color").text
+   rating =book.find_element(By.CLASS_NAME, "star-rating").get_attribute("class").replace("star-rating ", "")
+   stock = book.find_element(By.CSS_SELECTOR, "p.instock.availability").text.strip()
+   
+   book_info = {"Title":title, "Price":price, "Rating":rating, "Stock":stock}
+   books_data.append(book_info)
+
+for book in books_data:
+   print(f"Title:{book['Title']}")
+   print(f"Price:{book['Price']}")
+   print(f"Rating:{book['Rating']}")
+   print(f"Stock:{book['Stock']}")
+   print("-" * 40)
+
+df = pd.DataFrame(books_data)
+df.to_csv("books.csv", index=False)
+print("Data saved successfully!")
 driver.quit()
